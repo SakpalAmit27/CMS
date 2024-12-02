ffef6f767fb188c2bbab1781a42b6b8f39b1b3ee
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-02 19:41:59 +0530

    added customnavbar (may be temporary for style) along with some dir changes to manage files and components

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>opencms</title>
+   <link rel="stylesheet" href="includes/components/navbar/navbar.css">
+   <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.14/dist/full.min.css" rel="stylesheet" type="text/css" />
+   <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+   
+</body>
+</html>
+
 <?php
    
+
+   include('includes/components/navbar/navbar.php');
    include('includes/database.php');
 
-   echo "this is mycms"
+   echo "welcome to opencms"
 ?>
