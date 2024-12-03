591ac8cb8c743ec89def8e243e0098bf42931365
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-03 23:35:34 +0530

    common commit

--- index.php
+++ index.php
@@ -12,14 +12,14 @@ include('includes/components/loginform/loginform.php');
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>opencms</title>
+   <title>openspear</title>
    <link rel="stylesheet" href="includes/components/navbar/navbar.css">
    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.14/dist/full.min.css" rel="stylesheet" type="text/css" />
    <script src="https://cdn.tailwindcss.com"></script>
    <link rel="stylesheet" href="includes/components/footer/footer.css">
 </head>
 <body>
-   <!-- Main content -->
+
 </body>
 </html>
 
