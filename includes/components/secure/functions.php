c861996c3e5031a68a9f5f9cee2d20c4fa01c532
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-09 15:13:37 +0530

    created functions to get and set message through the files

--- includes/components/secure/functions.php
+++ includes/components/secure/functions.php
@@ -8,4 +8,22 @@
         die();
     }
  }
+
+
+ function set_message($message){
+    {
+        $_SESSION['message'] = $message;
+
+    }
+ }
+
+ function get_message(){
+    if(isset($_SESSION['message'])){
+        echo '<p>' . $_SESSION['message'] . '</p>'; 
+        unset($_SESSION['message']);
+        
+    }
+ }
+
 ?>
+
