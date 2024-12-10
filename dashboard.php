7fff10b5f09f49f2f8ed8b6dd6058e5dad05d508
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-10 14:52:51 +0530

    added a navigation for dashboard to user and post management

--- dashboard.php
+++ dashboard.php
@@ -18,12 +18,38 @@
     <title>Dashboard</title>
 </head>
 <body>
-    <div>
-        <h1 class="text-center text-4xl font-bold">Dashboard</h1>
-        
-    </div>
+        <div class="text-center items-center justify-center mt-10" style="height: 100vh;">
+            <h1 class="display-1 mb-4">Dashboard</h1>
+            <div class="flex flex-row gap-6 items-center justify-center text-center list-container">
+            <span><a href="users.php">Users management</a></span>
+            <span> | </span>
+            <span><a href="posts.php">Posts management</a></span>
+
+            </div>
+        </div>
+
 
     <?php include('./includes/components/footer/footer.php') ?>
 </body>
+
+
+
+<style>
+
+   @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Eczar&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Oswald:wght@600&family=Permanent+Marker&family=Pixelify+Sans:wght@400..700&family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&display=swap');
+
+
+    .display-1{
+    font-family:raleway;
+    font-weight:800;
+    font-size:1.7rem;
+    }
+
+    .list-container{
+        font-family:inter;
+        font-weight:500;
+        font-size:1rem;
+    }
+</style>
 </html>
 
