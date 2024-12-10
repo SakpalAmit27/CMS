fb07397fdeed8be2de0c4784a60c7be9b073555a
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-10 17:12:06 +0530

    created users.php to let the person manage users

---
+++ users.php
@@ -0,0 +1,55 @@
+
+<?php 
+
+    session_start();    
+
+    include('./includes/components/navbar/navbar.php');
+    
+
+    include_once('./includes/components/secure/functions.php');
+    secure();
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users management</title>
+</head>
+<body>
+        <div class="text-center items-center justify-center mt-10" style="height: 100vh;">
+            <h1 class="display-1 mb-4">USERS MANAGEMENT</h1>
+            <div class="flex flex-row gap-6 items-center justify-center text-center list-container">
+            <span><a href="users.php">Users management</a></span>
+            <span> | </span>
+            <span><a href="posts.php">Posts management</a></span>
+
+            </div>
+        </div>
+
+
+    <?php include('./includes/components/footer/footer.php') ?>
+</body>
+
+
+
+<style>
+
+   @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Eczar&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Oswald:wght@600&family=Permanent+Marker&family=Pixelify+Sans:wght@400..700&family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&display=swap');
+
+
+    .display-1{
+    font-family:raleway;
+    font-weight:800;
+    font-size:1.7rem;
+    }
+
+    .list-container{
+        font-family:inter;
+        font-weight:500;
+        font-size:1rem;
+    }
+</style>
+</html>
+
